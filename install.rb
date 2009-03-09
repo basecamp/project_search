6f8e3a7b9ba75df0886155c6141a50f86d52eceb
Jamis Buck <jamis@37signals.com>
2009-03-09 08:59:29 -0600

    update installer to move just the script we need

--- install.rb
+++ install.rb
@@ -1,6 +1,6 @@
 require 'fileutils'
 
-source = File.join(RAILS_ROOT, 'vendor', 'plugins', 'project_search', 'script')
-target = File.join(RAILS_ROOT, 'script')
+source = File.join(RAILS_ROOT, 'vendor', 'plugins', 'project_search', 'script', 'find')
+target = File.join(RAILS_ROOT, 'script', 'find')
 
 FileUtils.cp_r(source, target)
